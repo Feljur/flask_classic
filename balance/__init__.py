ddf8d449e539f08bf960ae9f26ac4b510878b1ee
Felipe Jurado <hola@felipejurado.com>
2021-09-18 17:28:34 +0200

    Añadido archivos estaticos de plantilla

--- balance/__init__.py
+++ balance/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 FICHERO = 'data/movimientos.csv'
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
 
 from balance import views
--- balance/views.py
+++ balance/views.py
@@ -1,10 +1,24 @@
 from balance import app
 from flask import render_template
 from balance.models import ListaMovimientos
+from flask import url_for
+from datetime import date, datetime
+
+
+def fecha_actual(date):
+    months = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+    day = date.day
+    month = months[date.month - 1]
+    year = date.year
+    fecha = "{} de {} de {}".format(day, month, year)
+
+    return fecha
+
 
 
 @app.route('/')
 def inicio():
     lm = ListaMovimientos()
     lm.leer()
-    return render_template('inicio.html', titulo="Balance de Cuentas", items=lm.movimientos)
+    now = datetime.now()
+    return render_template('inicio.html', titulo="Balance de Cuentas", items=lm.movimientos, fecha_hoy = fecha_actual(now))
